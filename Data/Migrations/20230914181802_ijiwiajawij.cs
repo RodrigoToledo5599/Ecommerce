eaf34cdd12ab04eb52a6943b732629eaddd8ae30
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-09-14 16:39:58 -0300

    database changes

---
+++ Data/Migrations/20230914181802_ijiwiajawij.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ijiwiajawij : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Account_Role_RoleId",
+                table: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Account_RoleId",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Account");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Account",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Account",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Role",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Account");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Account",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(63)", maxLength: 63, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Account",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RoleId",
+                value: null);
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Cliente" },
+                    { 3, "Vendedor" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RoleId",
+                table: "Account",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Account_Role_RoleId",
+                table: "Account",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id");
+        }
+    }
+}
